7af742e335015365d6ec59af837750afcdf18655
Bunny Bharadwaj <93427371+Bharadwaj-Mahadas@users.noreply.github.com>
2025-04-01 14:43:49 +0530

    extra functions

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 print("hello world")
 text = "in small letter"
 print(text.upper())
+k= input("enter any string")
+
+print(len(k))
+
+print("the position at which the character is present is " + str(k.index("l")))
