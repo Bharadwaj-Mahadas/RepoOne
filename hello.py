1db470aaada09756db4e6b5721a78fa60bf34a18
Bunny <Bunny@gmail.com>
2025-04-07 16:20:27 +0530

    list function handson

--- hello.py
+++ hello.py
@@ -1,12 +1,40 @@
-print("hello world")
-text = "in small letter"
-print(text.upper())
-k= input("enter any string")
+# print("hello world")
+# text = "in small letter"
+# print(text.upper())
+# k= input("enter any string")
+#
+# print(len(k))
+#
+# k= input("enter any string")
+#
+# print(len(k))
+#
+# print("the position at which the character is present is " + str(k.index("l")))
+#
+# #list functions
 
-print(len(k))
 
-k= input("enter any string")
+a = [10,20,30,40,50]
+print(a.index(20))
 
-print(len(k))
+b=a.copy()
 
-print("the position at which the character is present is " + str(k.index("l")))
+a.extend(b)
+a.append("bunny")
+print(a)
+
+a.pop()
+print(a)
+
+a.insert(2,"Hello")
+print(a)
+
+b.clear()
+print(b)
+
+b= a.copy()
+print(b)
+
+print(b.index('Hello'))
+
+
